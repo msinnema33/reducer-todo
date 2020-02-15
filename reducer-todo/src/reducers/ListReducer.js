8ffe486e441f2f8e4492215f921812a384eb5d15
Mike Sinnema <msinnema33@gmail.com>
2020-02-15 15:24:37 -0700

    working on reducer

---
+++ reducer-todo/src/reducers/ListReducer.js
@@ -0,0 +1,14 @@
+import React, { useReducer } from 'react'
+
+const initialState = {
+    item: 'Learn about reducers',
+    completed: false,
+    id: 3892987589
+}
+
+
+ const itemReducer = (state, action) => {
+    return
+}
+
+
